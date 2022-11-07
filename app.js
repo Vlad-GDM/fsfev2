c36fcc1ef0e4969296f12bf5598448e58c4b66a3
vladalex1209 <vlad.pirvan@gdm.ro>
2022-11-07 18:10:02 +0200

    cu logger cu tot

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
 const port = 3000;
+express.use(logger('dev'));
 
 app.get('/', (req, res) => { res.send('Hello World!') })
-app.get('/users/id', (req, res) => {
+app.get('/users/:id', (req, res) => {
   res.send('Ai vrut sa accesezi pagina de user cu id-ul ' + req.params.id)
 })
 
