eefe87371c9bacec198fd29790a018dd334d88d7
vladalex1209 <vlad.pirvan@gdm.ro>
2022-11-08 11:12:18 +0200

    changed back to app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const express = require('express');
-const app = express();
-const logger = require('morgan');
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.use(logger('dev'));
-
-app.get('/', (req, res) => { res.send('Hello Mom!') })
-
-app.get('/users/:id', (req, res) => {
-  res.send('Ai vrut sa accesezi pagina de user cu id-ul ' + req.params.id)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-
-app.listen(port, () => { console.log(`Example app listening on port ${port}`) })	
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})	
